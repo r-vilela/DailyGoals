337d37726ed2c6104e6cb558a8e41059a096c393
Vilela UNIR <raulvilela29@gmail.com>
2025-08-29 14:59:27 -0400

    feat: Adding goals and progress bar working

--- app/src/main/java/com/example/dailygoals/MainActivity.java
+++ app/src/main/java/com/example/dailygoals/MainActivity.java
@@ -1,15 +1,19 @@
 package com.example.dailygoals;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -33,31 +37,63 @@ public class MainActivity extends AppCompatActivity {
 
         EditText txtInputGoals = findViewById(R.id.txtInputGoals);
         Button btnGoals = findViewById(R.id.btnGoals);
+        ProgressBar proBarGoals = findViewById(R.id.proBarGoals);
+
+        proBarGoals.setMax(goalsList.size());
+        proBarGoals.setProgress(95);
+
         btnGoals.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                String mensagem = "Texto informado: ";
-                if (!txtInputGoals.getText().toString().isEmpty()){
-                    Toast.makeText(MainActivity.this, mensagem + txtInputGoals.getText().toString(), Toast.LENGTH_SHORT).show();
-                }
 
+                CheckBox check = new CheckBox(MainActivity.this);
+                check.setText(txtInputGoals.getText().toString());
+
+                goalsList.add(check);
+                addGoals(goalsList, proBarGoals);
             }
         });
     }
 
-    protected void addGoals(ArrayList<CheckBox> goalsList){
+    protected void addGoals(ArrayList<CheckBox> goalsList,  ProgressBar proBarGoals){
         LinearLayout linearGoals = findViewById(R.id.linearGoals);
 
-        LinearLayout.LayoutParams layoutParam = new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.WRAP_CONTENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
         );
+        float density = getResources().getDisplayMetrics().density;
+        params.setMargins((int)(16 * density), (int)(8 * density), (int)(16 * density), (int)(8 * density));
+
+        Typeface myFont = ResourcesCompat.getFont(this, R.font.adlam_display);
+
 
         for(CheckBox checkbx : goalsList){
-            checkbx.setLayoutParams(layoutParam);
+            linearGoals.removeView(checkbx);
 
-            linearGoals.addView(checkbx);
+            checkbx.setOnClickListener((View v) ->{
+
+
+                CheckBox check = (CheckBox) v;
+
+                if(check.isChecked()) {
+                    System.out.println("COOLL");
+                    proBarGoals.setProgress(proBarGoals.getProgress() + 1);
+                } else{
+                    proBarGoals.setProgress(proBarGoals.getProgress() - 1);
+                }
+
+
+            });
+
+            checkbx.setTypeface(myFont);
+
+            linearGoals.addView(checkbx, params);
 
         }
+
+        proBarGoals.setMax(goalsList.size());
     }
+
+
 }
